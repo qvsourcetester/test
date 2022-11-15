d1820162a5ba003e140fa321bd48abac98bdb1ef
qvsourcetester <qvsourcetester@gmail.com>
2022-11-15 15:36:19 +0000

    Add bad code to test CodeScanningAlerts table

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("index");
+
+var user = "loser";
+// Example from here https://codeql.github.com/codeql-query-help/javascript/js-insecure-randomness/
+// ...for testing new CodeScanningAlerts table.
+// BAD: the random suffix is not cryptographically secure
+var suffix = Math.random();
+var password = "myPassword" + suffix;
+
+console.log("You have generated a very bad password! Congratulations " + user +"!");
